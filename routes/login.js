065abf6dce205a847eb524f2406f75facd17d593
BenjaminDexant <benjamin.dexant@gmail.com>
2020-07-23 11:19:09 +0200

    login route

---
+++ routes/login.js
@@ -0,0 +1,41 @@
+const express = require("express");
+
+const router = express.Router({ mergeParams: true });
+
+const util = require("util");
+const bcrypt = require("bcrypt");
+
+const connection = require("../db");
+
+const queryAsync = util.promisify(connection.query).bind(connection);
+
+router.post('/', async (req, res) => {
+  const { name, password } = req.body;
+  try {
+    const users = await queryAsync('SELECT * FROM Admin WHERE name = ?', name);
+    if (!users) {
+      return res.status(500).json({
+        status: 'error',
+        errorMessage: 'Data does not match',
+      });
+    }
+    const checkPass = bcrypt.compareSync(password, users[0].password);
+    delete users[0].password;
+    if (checkPass) {
+      return res.status(200).json({
+        ...users[0],
+      });
+    }
+    return res.status(500).json({
+      status: 'error',
+      errorMessage: 'Invalid data',
+    });
+  } catch (err) {
+    return res.status(500).json({
+      status: 'err',
+      errorMessage: 'Something went wrong',
+    });
+  }
+});
+
+module.exports = router;
