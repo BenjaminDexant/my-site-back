7dc7dcff66d30db5379fa4d3aac394b89bef93c6
BenjaminDexant <benjamin.dexant@gmail.com>
2020-07-23 09:54:20 +0200

    inscription bcrypt

--- routes/admin.js
+++ routes/admin.js
@@ -2,12 +2,45 @@ const express = require("express");
 
 const router = express.Router({ mergeParams: true });
 
-const connection = require('../db');
+const util = require("util");
+const bcrypt = require("bcrypt");
 
-router.get("/", (req, res) => {
-  connection.query("SELECT * FROM Admin", (error, results) => {
-    res.status(200).json(results);
-  });
+const connection = require("../db");
+
+const queryAsync = util.promisify(connection.query).bind(connection);
+
+const saltRounds = 10;
+
+// inscription
+router.post("/", async (req, res) => {
+  const { password } = req.body;
+  const salt = bcrypt.genSaltSync(saltRounds);
+  const hash = bcrypt.hashSync(password, salt);
+  const inscData = {
+    ...req.body,
+    password: hash,
+  };
+  try {
+    const { name } = req.body;
+    const users = await queryAsync("SELECT * FROM Admin WHERE name = ?", name);
+    if (users[0]) {
+      return res.status(409).json({
+        status: "error",
+        errorMessage: "User already exist",
+      });
+    }
+    const query = "INSERT INTO Admin SET ?";
+    const result = await queryAsync(query, inscData);
+    return res.status(201).json({
+      id_admin: result.insertId,
+      ...inscData,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      status: "error",
+      errorMessage: "Something went wrong",
+    });
+  }
 });
 
 module.exports = router;
